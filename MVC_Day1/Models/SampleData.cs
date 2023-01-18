c6364fe3e34d48622414f28aab9f27c9860f0885
fatma950 <fatmagomaa755@gmail.com>
2023-01-18 23:44:51 +0200

    Add project files.

---
+++ MVC_Day1/Models/SampleData.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace MVC_Day1.Models
+{
+    public static class SampleData
+    {
+        private static List<People> peoples = new List<People>()
+        {
+            new People()
+            {
+            Name= "Ali",
+            Email= "Ali@gmail.com",
+            Phone="0123456755",
+            WillAttend=true
+            },
+            new People()
+            {
+            Name= "Ahmed",
+            Email= "Ahmed@gmail.com",
+            Phone="0123456985",
+            WillAttend=false
+            }
+        };
+        public static List<People> Peoples { get => peoples; }
+        public static void AddPeople(People people)
+        {
+            peoples.Add(people);
+        }
+    }
+}
+
